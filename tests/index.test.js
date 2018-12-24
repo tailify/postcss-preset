cfb68057e3008a2bba3d95f7770760f23901112e
Jamie Connolly <jamie.connolly@tailify.com>
2018-12-24 11:41:14 +0000

    Use async/await instead of Promise.then in the tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,11 +3,9 @@
 const postcss = require('postcss');
 const preset = require('./../lib/index.js');
 
-test('should not contain invalid rules', () => {
-  return postcss([preset])
-    .process('@media (width >= 768px) {}\n', { from: 'foo/bar.css' })
-    .then(result => {
-      expect(result).toBeTruthy();
-      expect(result.css).toBe('@media (min-width: 768px) {}\n');
-    });
+test('should not contain invalid rules', async () => {
+  const processor = postcss([preset]);
+  const result = await processor.process('@media (width >= 768px) {}\n', { from: 'foo/bar.css' });
+  expect(result).toBeTruthy();
+  expect(result.css).toBe('@media (min-width: 768px) {}\n');
 });
