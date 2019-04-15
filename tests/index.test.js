93adbbae43495d3f6024f5765dfcdf3fce858ce3
Jamie Connolly <jamie@jamieconnolly.com>
2019-04-15 16:11:22 +0100

    Refactor unneeded plugin (#3)

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,28 @@
 'use strict';
 
 const postcss = require('postcss');
-const processor = postcss([require('./../lib/index.js')]);
+const postcssrc = require('postcss-load-config');
 
-const source = '@media (width >= 768px) {}\n';
+const source = `
+@media (width >= 768px) {
+  body {
+    background-color: red;
+  }
+}
+`;
 
 test('should not contain invalid rules', async () => {
+  const config = await postcssrc({}, './lib', { configPath: './lib/index.js' });
+  const processor = postcss(config);
+
   const result = await processor.process(source, { from: 'foo/bar.css' });
-  expect(result).toBeTruthy();
+  expect(result.css).toMatchSnapshot();
+});
+
+test('should minify when building for production', async () => {
+  const config = await postcssrc({ env: 'production' }, './lib', { configPath: './lib/index.js' });
+  const processor = postcss(config);
+
+  const result = await processor.process(source, { from: 'foo/bar.css' });
   expect(result.css).toMatchSnapshot();
 });
